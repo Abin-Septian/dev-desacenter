a44bb562a4b8dabdb44b1dad5f8f443137687e05
anwar fauzi <muhammadanwarfauzi1999@Gmail.com>
2021-06-01 05:23:02 +0700

    Update layoutsub.blade.php

--- resources/views/layoutsubmain/layoutsub.blade.php
+++ resources/views/layoutsubmain/layoutsub.blade.php
@@ -6,14 +6,6 @@
 
 
 
-    <script src="https://aframe.io/releases/1.2.0/aframe.min.js"></script>
-
-    <a-scene>
-     <a-box 
-     position = "0 1.5 -2"
-     color = "#550000"
-     >    
-     </a-box>
-    </a-scene>
+   
 
     @endsection
